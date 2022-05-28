f24d8f19a60185c10de791bd8e58c36a87911584
eduardogutierrez <gutierrez.dc@icloud.com>
2022-05-28 18:11:23 +0200

    Ejercicio 1 Parte 1 Terminado

---
+++ Ejercicio1OpenBootCamp/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+        public static void main(String[] args) {
+            int resultado = suma(2, 2, 3);
+
+            System.out.println("el resultado es:");
+            System.out.println(resultado);
+        }
+
+        public static int suma(int a, int b, int c) {
+            return a + b + c;
+        }
+    }
+
+
